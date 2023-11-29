fde521c9dc320313836a107730cd7f957889b171
admin <admin@DESKTOP-FEPU737>
2023-11-29 09:17:42 +0900

    게시판 충돌 해결.

--- hgjava/src/todo/p20231128/Board.java
+++ hgjava/src/todo/p20231128/Board.java
@@ -6,11 +6,9 @@ public class Board {
 	private String writer;
 	private String content;
 	private String writeDate;
-	
+
 	// 이 부분이 변경되었습니다.
-
 	// 나도 변경햇습니다..
-
 	
 	public Board(int boardNo, String title, String writer, String content, String writeDate) {
 
