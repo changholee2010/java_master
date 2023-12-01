cb9e998692005411b1061ad183ba58c4b574309b
admin <admin@DESKTOP-FEPU737>
2023-12-01 13:57:33 +0900

    인터페이스 끝.

--- hgjava/src/chap7/FriendExe.java
+++ hgjava/src/chap7/FriendExe.java
@@ -6,26 +6,39 @@ public class FriendExe {
 	// 1:등록 2:목록 3:조회
 
 	public boolean addFriend(Friend friend) {
-
+		for (int i = 0; i < storage.length; i++) {
+			if (storage[i] == null) {
+				storage[i] = friend;
+				return true;
+			}
+		}
 		return false;
 	}
 
 	public Friend[] friendList() {
-
-		return null;
+		return storage;
 	}
 
 	// 결과값이 null여부로 체크(X)
 	public Friend[] searchFriend(String name) {
 		// 반환유형이 정해져있음.
 		Friend[] result = new Friend[10];
-
+		int j = 0;
+		for (int i = 0; i < storage.length; i++) {
+			if (storage[i] != null && storage[i].getName().equals(name)) {
+				result[j++] = storage[i];
+			}
+		}
 		return result;
 	}
 
 	// 결과값이 null여부로 체크.
 	public Friend searchPhone(String phone) {
-
+		for (int i = 0; i < storage.length; i++) {
+			if (storage[i] != null && storage[i].getPhone().equals(phone)) {
+				return storage[i];
+			}
+		}
 		return null;
 	}
 
