1404205dedc6b92e41d8bee679d8085819efb73f
pc <pc@ydam.com>
2024-01-04 10:46:29 +0900

    map,set 처리후

--- BoardWeb/src/main/webapp/javascript/js/class4.js
+++ BoardWeb/src/main/webapp/javascript/js/class4.js
@@ -17,10 +17,38 @@ const friend = {
     }
 }
 friend.pets[0]['friends'][2] = '노량이';
-delete friend.age;
-friend.ages = 20;
+// delete friend.age;
+friend.ages = 20; // 속성추가.
 friend.showFriends = function () {
     console.log(this);
 }
 
 console.log(friend);
+
+// 참조값을 복사.
+const fcopy = friend;
+fcopy.age = 22;
+console.log(fcopy)
+
+// 객체를 복사.
+const fcopy2 = Object.assign({
+    bloodType: 'O'
+}, friend);
+fcopy2.age = 24;
+console.log(fcopy2);
+
+// 속성추가.
+Object.defineProperty(friend, 'height', {
+    get: function () {
+        return this._height;
+    },
+    set: function (value) {
+        if (value <= 0) {
+            alert('error');
+        } else {
+            this._height = value;
+        }
+    }
+});
+friend.height = 162;
+console.log(friend.height);
